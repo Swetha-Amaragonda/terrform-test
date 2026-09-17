54de79f0288385124260ab389b4eb82c9fef184d
Shwetha <amaragonda2002@gmail.com>
2026-09-17 15:58:59 +0530

    updatess2

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+locals {
+  instance_name = "terraform-github-actions-ec2"
+}
+
 data "aws_ami" "amazon_linux" {
   most_recent = true
 
@@ -14,11 +18,33 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+resource "null_resource" "delete_existing_instance" {
+  triggers = {
+    instance_name = local.instance_name
+    aws_region    = var.aws_region
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      $ids = aws ec2 describe-instances --region "${var.aws_region}" --filters "Name=tag:Name,Values=${local.instance_name}" "Name=instance-state-name,Values=pending,running,stopped,stopping" --query "Reservations[].Instances[].InstanceId" --output text
+
+      if (-not [string]::IsNullOrWhiteSpace($ids)) {
+        Write-Host "Terminating existing EC2 instance(s): $ids"
+        aws ec2 terminate-instances --region "${var.aws_region}" --instance-ids $ids
+      } else {
+        Write-Host "No existing EC2 instance found with tag Name=${local.instance_name}"
+      }
+    EOT
+  }
+}
+
 resource "aws_instance" "app" {
+  depends_on = [null_resource.delete_existing_instance]
+
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
 
   tags = {
-    Name = "terraform-github-actions-ec2"
+    Name = local.instance_name
   }
 }
